0c0b5686c170e96fd4930160b4ce01522b7b7923
D-Bibileishvili <d.bibileishvili19@gmail.com>
2025-05-15 11:53:00 +0400

    fetch products

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,62 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import NavBar from "@/components/NavBar/NavBar";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    console.log('fetching products..')
+    const response = await fetch("https://fakestoreapi.com/products");
+    const result = await response.json();
+    setProducts(result);
+
+    // console.log(result);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
-    <NavBar/>
-    
-      {/* <section className={styles.itemContainer}>
-        <div className={styles.imageContainer}>
-          <div className={styles.imageWrapper}>
-            <Image
-              src="/images/subaru.jpg"
-              width={240}
-              height={160}
-              className={styles.image}
-              alt="item image"
-            />
-          </div>
-        </div>
-
-        <div className={styles.description}>
-          <div>
-            <p className={styles.city}>თბილისი</p>
-          </div>
-          <div className={styles.nameWrapper}>
-            <h3>2014 - Subaru WRX</h3>
-            <Image src="/images/heart.png" width={16} height={16} alt="heart" />
-          </div>
-          <p className={styles.price}>33,900 ₾</p>
-          <div className={styles.about}>
-            <p>სედანი</p>
-            <p>ბენზინი</p>
-          </div>
-        </div>
-      </section> */}
+      {products.map((product) => (
+        <h3>{product.title}</h3>
+      ))}
     </>
   );
 }
+
+
+
+
+// {/* <section className={styles.itemContainer}>
+//         <div className={styles.imageContainer}>
+//           <div className={styles.imageWrapper}>
+//             <Image
+//               src="/images/subaru.jpg"
+//               width={240}
+//               height={160}
+//               className={styles.image}
+//               alt="item image"
+//             />
+//           </div>
+//         </div>
+
+//         <div className={styles.description}>
+//           <div>
+//             <p className={styles.city}>თბილისი</p>
+//           </div>
+//           <div className={styles.nameWrapper}>
+//             <h3>2014 - Subaru WRX</h3>
+//             <Image src="/images/heart.png" width={16} height={16} alt="heart" />
+//           </div>
+//           <p className={styles.price}>33,900 ₾</p>
+//           <div className={styles.about}>
+//             <p>სედანი</p>
+//             <p>ბენზინი</p>
+//           </div>
+//         </div>
+//       </section> */}
