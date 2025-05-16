73e28a45543433c508f309281303d6e8125900fa
D-Bibileishvili <d.bibileishvili19@gmail.com>
2025-05-16 15:56:38 +0400

    added increasing and decreasing quantity in the cart.

--- src/app/products/details/[id]/page.jsx
+++ src/app/products/details/[id]/page.jsx
@@ -1,8 +1,8 @@
 const page = async ({ params }) => {
-  const user = await params;
-  const response = await fetch(`https://fakestoreapi.com/users/3${user}`);
+  const id = await params;
+  const response = await fetch(`https://fakestoreapi.com/users/3${id}`);
   const product = await response.json();
-  return <h1>{product.id}</h1>;
+  return <h1>{product.title}</h1>;
 };
 
 export default page;
